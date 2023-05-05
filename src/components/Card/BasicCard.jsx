d0c565da255057e9aa0e18e1a378493d09d9023a
Jose Contreras <ji.ccdev@outlook.com>
2023-05-05 11:30:57 -0400

    feat: add basic card styles

--- src/components/Card/BasicCard.jsx
+++ src/components/Card/BasicCard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from '../../styles/components/Card/BasicCard.module.css';
 
 const BasicCard = ({ backgroundColor, data }) => {
   const { title, desc, icon } = data;
   return (
     <div
-      className={`${backgroundColor} md:h-56 xl:h-96 text-center py-6 px-4 my-5 xl:my-14 xl:mx-10 shadow-lg rounded-2xl cursor-pointer hover:shadow-xl trasition duration-300 ease-in-out`}
+      className={`${backgroundColor} ${styles.card} ${styles.education} md:h-56 xl:h-96 text-center py-6 px-4 my-5 xl:my-14 xl:mx-10 shadow-lg rounded-2xl cursor-pointer hover:shadow-xl trasition duration-300 ease-in-out`}
     >
+      <div className={`${styles.overlay}`}></div>
+
       <span className="flex items-center justify-center my-4">
         <i className="text-5xl xl:text-8xl text-teal-500">{icon}</i>
       </span>
