f6c2ca9d58bd5ef8a3217d0b724d2f949102e2ce
Jose Contreras <ji.ccdev@outlook.com>
2023-05-09 15:56:10 -0400

    feat: add new landing demos

--- src/data/demos.js
+++ src/data/demos.js
@@ -2,37 +2,37 @@ export const demosBasicData = [
   {
     id: 1,
     name: 'DEMO PAGINA BASICA 1',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631652/unne-media/images/2023-04-27_17_39_58-Inicio_-_P%C3%A1gina_b%C3%A1sica_2_z4zxnv.png',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-1_hha5o0.png',
     href: 'https://pagina-web-b1.partnersadvisers.info/',
   },
   {
     id: 2,
     name: 'DEMO PAGINA BASICA 2',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631752/unne-media/images/2023-04-27_17_42_12-Inicio_r6c9ke.png',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658408/accion-panal/demos/wb-2_gqxbdl.png',
     href: 'https://pagina-web-b2.partnersadvisers.info/',
   },
   {
     id: 3,
     name: 'DEMO PAGINA BASICA 3',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-3_om0eqp.png',
     href: 'https://pagina-web-b3.partnersadvisers.info/',
   },
   {
     id: 4,
     name: 'DEMO PAGINA BASICA 4',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-4_kozswv.png',
     href: 'https://pagina-web-b4.partnersadvisers.info/',
   },
   {
     id: 5,
     name: 'DEMO PAGINA BASICA 5',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658407/accion-panal/demos/wb-5_t0cmez.png',
     href: 'https://pagina-web-b5.partnersadvisers.info/',
   },
   {
     id: 6,
     name: 'DEMO PAGINA BASICA 6',
-    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1683658528/accion-panal/demos/wp-6_o0gehe.png',
     href: 'https://pagina-web-b6.partnersadvisers.info/',
   },
 ];
