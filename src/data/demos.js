c4d7e16638be76ce18d646024b6ad89d366493de
Jose Contreras <ji.ccdev@outlook.com>
2023-05-09 13:26:59 -0400

    feat: add new url pages demo

--- src/data/demos.js
+++ src/data/demos.js
@@ -3,43 +3,37 @@ export const demosBasicData = [
     id: 1,
     name: 'DEMO PAGINA BASICA 1',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631652/unne-media/images/2023-04-27_17_39_58-Inicio_-_P%C3%A1gina_b%C3%A1sica_2_z4zxnv.png',
-    href: 'https://pagina-web-b2.partnersadvisers.info/',
-    checked: false,
+    href: 'https://pagina-web-b1.partnersadvisers.info/',
   },
   {
     id: 2,
     name: 'DEMO PAGINA BASICA 2',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631752/unne-media/images/2023-04-27_17_42_12-Inicio_r6c9ke.png',
-    href: 'https://pagina-web-b3.partnersadvisers.info/',
-    checked: false,
+    href: 'https://pagina-web-b2.partnersadvisers.info/',
   },
   {
     id: 3,
     name: 'DEMO PAGINA BASICA 3',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
     href: 'https://pagina-web-b3.partnersadvisers.info/',
-    checked: false,
   },
   {
     id: 4,
     name: 'DEMO PAGINA BASICA 4',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
-    href: 'https://pagina-web-b3.partnersadvisers.info/',
-    checked: false,
+    href: 'https://pagina-web-b4.partnersadvisers.info/',
   },
   {
     id: 5,
     name: 'DEMO PAGINA BASICA 5',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
-    href: 'https://pagina-web-b3.partnersadvisers.info/',
-    checked: false,
+    href: 'https://pagina-web-b5.partnersadvisers.info/',
   },
   {
     id: 6,
     name: 'DEMO PAGINA BASICA 6',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
-    href: 'https://pagina-web-b3.partnersadvisers.info/',
-    checked: false,
+    href: 'https://pagina-web-b6.partnersadvisers.info/',
   },
 ];
 
@@ -61,5 +55,26 @@ export const demosAdvancedData = [
     name: 'DEMO PAGINA AVANZADA 3',
     src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
     href: 'https://pagina-web-b3.partnersadvisers.info/',
+  },
+];
+
+export const demosLandingsData = [
+  {
+    id: 1,
+    name: 'DEMO LANDING PAGE 1',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631652/unne-media/images/2023-04-27_17_39_58-Inicio_-_P%C3%A1gina_b%C3%A1sica_2_z4zxnv.png',
+    href: 'https://landing-1.partnersadvisers.info/',
+  },
+  {
+    id: 2,
+    name: 'DEMO LANDING PAGE 2',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1682631752/unne-media/images/2023-04-27_17_42_12-Inicio_r6c9ke.png',
+    href: 'https://landing-2.partnersadvisers.info/',
+  },
+  {
+    id: 3,
+    name: 'DEMO LANDING PAGE 3',
+    src: 'https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg',
+    href: 'https://landing-3.partnersadvisers.info/',
   },
 ];
