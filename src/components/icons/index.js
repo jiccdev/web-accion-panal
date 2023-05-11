40f91d4eae96098e66b2988dfd2f82721660fb77
ManuelMontenegroVerdugo <m.montenegro.verdugo@gmail.com>
2023-05-11 17:37:17 -0400

    feature:Icons

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,60 +1,59 @@
-import { IoIosArrowDown } from 'react-icons/io';
-import { RiUserSearchFill, RiPencilRulerLine } from 'react-icons/ri';
 import {
+  AiTwotoneGold,
+  AiOutlineShareAlt,
+  AiFillPrinter,
+} from 'react-icons/ai';
+import { GrClose } from 'react-icons/gr';
+import {
+  MdOutlineArrowDropDown,
+  MdSimCardDownload,
+  MdWeb,
+  MdDevices,
+} from 'react-icons/md';
+import { BsFacebook, BsLinkedin } from 'react-icons/bs';
+import { BsInstagram, BsFillPlayFill } from 'react-icons/bs';
+import {
+  BsYoutube,
   BsBarChartFill,
-  BsCheckCircle,
-  BsFillCalendarCheckFill,
-} from 'react-icons/bs';
-import { GiMoneyStack, GiHomeGarage } from 'react-icons/gi';
-import { TbPigMoney } from 'react-icons/tb';
-import { TbArrowBigRightFilled } from 'react-icons/tb';
-import { TbArrowBigDownFilled } from 'react-icons/tb';
-import { TbCircleNumber1 } from 'react-icons/tb';
-import { TbCircleNumber2 } from 'react-icons/tb';
-import { TbCircleNumber3 } from 'react-icons/tb';
-import { TbCircleNumber4 } from 'react-icons/tb';
-import { TbSquareRoundedNumber1Filled } from 'react-icons/tb';
-import { TbSquareRoundedNumber2Filled } from 'react-icons/tb';
-import { TbSquareRoundedNumber3Filled } from 'react-icons/tb';
-import { TbSquareRoundedNumber4Filled } from 'react-icons/tb';
-
-import { IoIosArrowBack } from 'react-icons/io';
-import { IoIosArrowForward, IoClose } from 'react-icons/io';
-import { HiClipboard, HiClipboardCheck } from 'react-icons/hi';
-
-import {
-  FaUserAlt,
-  FaShare,
-  FaBed,
-  FaBath,
-  FaThList,
-  FaMapMarked,
-  FaMapMarkerAlt,
-} from 'react-icons/fa';
-import { BsFillTelephoneFill, BsSearch, BsFillPlayFill } from 'react-icons/bs';
-import { FiMail } from 'react-icons/fi';
-import {
+  BsFillTelephoneFill,
   BsFillHouseAddFill,
+  BsCheckCircle,
+  BsSearch,
+  BsFillCalendarCheckFill,
   BsFillGridFill,
+  BsPersonCircle,
   BsFillInboxFill,
 } from 'react-icons/bs';
-import { BiBookOpen, BiPaperPlane } from 'react-icons/bi';
-import { AiOutlineShareAlt, AiFillPrinter } from 'react-icons/ai';
-import { AiTwotoneGold } from 'react-icons/ai';
-import { GrClose } from 'react-icons/gr';
-import { MdOutlineArrowDropDown, MdSimCardDownload } from 'react-icons/md';
-import { BsFacebook } from 'react-icons/bs';
-import { BsInstagram } from 'react-icons/bs';
-import { BsYoutube } from 'react-icons/bs';
 import { MdViewList } from 'react-icons/md';
+import { FaCogs, FaUserAlt, FaThList, FaMapMarkerAlt } from 'react-icons/fa';
+import { GiProgression, GiMoneyStack } from 'react-icons/gi';
+import {
+  IoIosPeople,
+  IoIosArrowDown,
+  IoIosArrowBack,
+  IoIosArrowForward,
+  IoClose,
+} from 'react-icons/io';
+import { RiUserSearchFill, RiPencilRulerLine } from 'react-icons/ri';
+import {
+  TbPigMoney,
+  TbArrowBigRightFilled,
+  TbArrowBigDownFilled,
+  TbSquareRoundedNumber1Filled,
+  TbSquareRoundedNumber2Filled,
+  TbSquareRoundedNumber3Filled,
+  TbSquareRoundedNumber4Filled,
+} from 'react-icons/tb';
+import { FiMail } from 'react-icons/fi';
+import { BiBookOpen, BiPaperPlane } from 'react-icons/bi';
+import { FaShare, FaBed, FaBath, FaMapMarked } from 'react-icons/fa';
+import { GiHomeGarage } from 'react-icons/gi';
+import { HiClipboard, HiClipboardCheck } from 'react-icons/hi';
 import { GoSearch } from 'react-icons/go';
 import { SiFastly } from 'react-icons/si';
-import { MdWeb } from 'react-icons/md';
-import { BsPersonCircle } from 'react-icons/bs';
-import { MdDevices } from 'react-icons/md';
-import { FaCogs } from 'react-icons/fa';
-import { GiProgression } from 'react-icons/gi';
-import { IoIosPeople } from 'react-icons/io';
+import { RiFacebookCircleLine } from 'react-icons/ri';
+import { TiSocialLinkedinCircular } from 'react-icons/ti';
+import { AiOutlinePlus, AiOutlineWhatsApp, AiFillPhone } from 'react-icons/ai';
 import { GiArtificialIntelligence } from 'react-icons/gi';
 import { MdSecurity } from 'react-icons/md';
 import { GiReceiveMoney } from 'react-icons/gi';
@@ -113,6 +112,58 @@ export const iconsList = {
   FaCogs,
   GiProgression,
   IoIosPeople,
+  IoIosArrowDown,
+  RiUserSearchFill,
+  GiMoneyStack,
+  TbPigMoney,
+  TbArrowBigRightFilled,
+  TbArrowBigDownFilled,
+  TbSquareRoundedNumber1Filled,
+  TbSquareRoundedNumber2Filled,
+  TbSquareRoundedNumber3Filled,
+  TbSquareRoundedNumber4Filled,
+  FaUserAlt,
+  BsFillTelephoneFill,
+  FiMail,
+  BsFillHouseAddFill,
+  BiBookOpen,
+  AiOutlineShareAlt,
+  AiTwotoneGold,
+  GrClose,
+  MdOutlineArrowDropDown,
+  BsFacebook,
+  BsInstagram,
+  BsYoutube,
+  BsCheckCircle,
+  BsSearch,
+  FaShare,
+  AiFillPrinter,
+  RiPencilRulerLine,
+  FaBed,
+  FaBath,
+  BsFillCalendarCheckFill,
+  GiHomeGarage,
+  BsFillGridFill,
+  FaThList,
+  FaMapMarked,
+  FaMapMarkerAlt,
+  IoIosArrowBack,
+  IoIosArrowForward,
+  HiClipboard,
+  HiClipboardCheck,
+  GoSearch,
+  MdSimCardDownload,
+  SiFastly,
+  BsFillPlayFill,
+  IoClose,
+  BiPaperPlane,
+  BsFillInboxFill,
+  RiFacebookCircleLine,
+  TiSocialLinkedinCircular,
+  BsLinkedin,
+  AiOutlinePlus,
+  AiOutlineWhatsApp,
+  AiFillPhone,
   GiArtificialIntelligence,
   MdSecurity,
   GiReceiveMoney,
