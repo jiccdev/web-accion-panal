35b0d77662e134b4385a58491ea80baac4129097
Jose Contreras <ji.ccdev@outlook.com>
2023-05-16 17:15:46 -0400

    fix: modified form data

--- src/components/PageSections/Contact/components/FormSection.jsx
+++ src/components/PageSections/Contact/components/FormSection.jsx
@@ -9,7 +9,7 @@ const FormSection = () => {
                 <div className="max-w-screen-xl m-0 sm:m-20 bg-white shadow sm:rounded-lg flex justify-center flex-1">
                     <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
                         <Form 
-                            realtorEmail='jos.delalamo@gmail.com' 
+                            realtorEmail='' 
                         /> 
                     </div>
                     <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
