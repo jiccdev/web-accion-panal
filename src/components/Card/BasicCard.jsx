2f7c0c855d424417b2ecf1f16edd64d0177c9441
Jose Contreras <ji.ccdev@outlook.com>
2023-05-17 12:54:55 -0400

    feat: add react-awesome-reveal deps

--- src/components/Card/BasicCard.jsx
+++ src/components/Card/BasicCard.jsx
@@ -1,23 +1,42 @@
 import React from 'react';
+import Reveal from 'react-awesome-reveal';
 import Image from 'next/image';
 import styles from '../../styles/components/Card/BasicCard.module.css';
+import { keyframes } from '@emotion/react';
+
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(80px);
+    transform: translateY(80px);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+`;
 
 const BasicCard = ({ backgroundColor, data }) => {
   const { title, desc, icon } = data;
   return (
-    <div
-      className={`${backgroundColor} ${styles.card} ${styles.education} md:h-56 xl:h-[410px] text-center py-6 px-4 my-5 xl:my-14 xl:mx-10 shadow-lg rounded-2xl cursor-pointer hover:shadow-xl trasition duration-300 ease-in-out`}
-    >
-      <div className={`${styles.overlay}`}></div>
-      <span className="flex items-center justify-center my-4">
-        <Image src={icon} height={130} width={130} alt={`icon-${title}`} />
-      </span>
+    <Reveal keyframes={fadeInUp} delay={300} duration={600} triggerOnce>
+      <div
+        className={`${backgroundColor} ${styles.card} ${styles.education} md:h-56 xl:h-[410px] text-center py-6 px-4 my-5 xl:my-14 xl:mx-10 shadow-lg rounded-2xl cursor-pointer hover:shadow-xl trasition duration-300 ease-in-out`}
+      >
+        <div className={`${styles.overlay}`}></div>
+        <span className="flex items-center justify-center my-4">
+          <Image src={icon} height={130} width={130} alt={`icon-${title}`} />
+        </span>
 
-      <h3 className="text-xl h-5 mb-2 font-bold text-gray-800 uppercase">{title}</h3>
-      <p className="h-auto xl:h-36 text-md mt-4 mb-2 xl:text-lg text-gray-700 w-[90%] mx-auto">
-        {desc}
-      </p>
-    </div>
+        <h3 className="text-xl h-5 mb-2 font-bold text-gray-800 uppercase">
+          {title}
+        </h3>
+        <p className="h-auto xl:h-36 text-md mt-4 mb-2 xl:text-lg text-gray-700 w-[90%] mx-auto">
+          {desc}
+        </p>
+      </div>
+    </Reveal>
   );
 };
 
--- src/components/PageSections/Home/components/Hero.jsx
+++ src/components/PageSections/Home/components/Hero.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import Reveal from 'react-awesome-reveal';
 import Image from 'next/image';
 import Section from '@/components/Section/Section';
 import Button from '@/components/Button/Button';
 import HeroImg from '../../../../assets/img/hero/Diseño sin título.svg';
+import { keyframes } from '@emotion/react';
+
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(90px);
+    transform: translateY(90px);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+`;
 
 const Hero = () => (
   <Section className="relative">
@@ -10,22 +25,32 @@ const Hero = () => (
       id="inicio-section"
       className="grid grid-cols-1 xl:grid-cols-3 gap-4 place-items-center justify-items-start"
     >
-      <div className="flex-shrink-0 mb-10 lg:mb-0 lg:w-full mx-2 xl:mx-0 ml-0 lg:ml-24 xl:ml-24">
-        <h1 className="text-2xl font-bold xl:text-5xl text-teal-500">
-          LA EVOLUCIÓN INMOBILIARIA A UN CLICK
-        </h1>
+      <Reveal effect="fadeInUp" duration={1000} delay={500}>
+        <div className="flex-shrink-0 mb-10 lg:mb-0 lg:w-full mx-2 xl:mx-0 ml-0 lg:ml-24 xl:ml-24">
+          <h1 className="text-2xl font-bold xl:text-5xl text-teal-500">
+            LA EVOLUCIÓN INMOBILIARIA A UN CLICK
+          </h1>
 
-        <span className="block mt-5 text-md xl:text-xl text-gray-800">
-          Desarrollamos software de gestión y las herramientas mas innovadoras
-          del mercado para optimizar el corretaje de propiedades
-        </span>
+          <span className="block mt-5 text-md xl:text-xl text-gray-800">
+            Desarrollamos software de gestión y las herramientas mas innovadoras
+            del mercado para optimizar el corretaje de propiedades
+          </span>
 
-        <div className="my-5">
-          <Button>Más información</Button>
+          <div className="my-5">
+            <Button>Más información</Button>
+          </div>
         </div>
-      </div>
+      </Reveal>
+
       <div className="col-span-2">
-        <Image src={HeroImg} alt="Displays" width="full" height="full" className='' />
+        <Reveal keyframes={fadeInUp} delay={300} duration={600}>
+          <Image
+            src={HeroImg}
+            alt="hero-main-laptop"
+            width="full"
+            height="full"
+          />
+        </Reveal>
       </div>
     </div>
   </Section>
