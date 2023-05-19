f023fd25898f4522cb74ab2725e4a1ae6f57f690
Jose Contreras <ji.ccdev@outlook.com>
2023-05-19 15:31:58 -0400

    feat: add diagram

--- src/components/PageSections/Services/components/Kanjeo/Diagrama.jsx
+++ src/components/PageSections/Services/components/Kanjeo/Diagrama.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import Image from 'next/image'
 import Section from '@/components/Section/Section'
 import HeadingSection from '@/components/HeadingSection/HeadingSection'
+import DiagramUi from '../../../../../assets/img/diagram/diagram.svg'
+import { keyframes } from '@emotion/react';
+import Reveal from 'react-awesome-reveal';
+
+
+const fadeInUp = keyframes`
+  0% {
+    opacity: 0;
+    -webkit-transform: translateY(90px);
+    transform: translateY(90px);
+  }
+  100% {
+    opacity: 1;
+    -webkit-transform: translateY(0);
+    transform: translateY(0);
+  }
+`;
+
 function Diagrama() {
     return (
 
 
-        <div className='flex justify-center items-center mt-32'>
+        <div className='flex justify-center items-center mt-32 flex-col'>
             <div className='max-w-[1200px] w-full text-center items-center'>
                 <HeadingSection
                     title="DIAGRAMA O DEMO DE COMO FUNCIONA"
@@ -14,6 +33,15 @@ function Diagrama() {
                     color="violet"
                 />
             </div>
+
+            <div className="flex justify-center items-center">
+                <Reveal effect={fadeInUp} duration={1000} delay={500}>
+                    <div className="w-[100%]">
+                        <Image src={DiagramUi} width="full" height="full" alt="img-diagram" className="w-[100%]" />
+                    </div>
+                </Reveal>
+            </div>
+
         </div>
 
     )
